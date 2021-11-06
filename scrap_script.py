e17e2608aa0082037e24b307fcf794b7d447f70d
Bartek <bartosz.dabrowski2000@gmail.com>
2021-11-06 15:11:00 +0100

    Updated scrap script

--- scrap_script.py
+++ scrap_script.py
@@ -1,66 +1,93 @@
+import json
+
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 
 
-#zwraca pole danej specyfikacji (wynika z budowy strony)
-def findSpec(specToFind):
-	specifications = itemsoup.find(class_='DaneLista KolorLista')
-	correctTag = specifications.find(text=specToFind)
-	if(correctTag):
-		return correctTag.findNext('strong').get_text(strip=True)
-	return ''
+def get_categories(lamps, baseurl, categories_links):
+    req = requests.get(f'{baseurl}/lampy-sufitowe-c-760.html')
+    soup = BeautifulSoup(req.content, 'lxml')
+    categories = soup.find_all('li', class_='OknoRwd')
+
+    for category in categories:
+        link = category.find_all('a', href=True)[0]['href']
+        name = category.find('span').get_text(strip=True)
+        lamps[name] = {}
+        categories_links.append(link)
 
 
+def findSpec(itemsoup, specToFind):
+    specifications = itemsoup.find(class_='DaneLista KolorLista')
+    if specifications:
+        correctTag = specifications.find(text=specToFind)
+        if correctTag:
+            return correctTag.findNext('strong').get_text(strip=True)
+    return ''
 
-baseurl = "https://polskielampy.pl"
 
-req = requests.get(baseurl + '/lampy-sufitowe-c-760.html/s=1')
+def get_products(baseurl, lamps, categories_links):
+    productlinks = []
+    keys_list = list(lamps.keys())
+    for i in range(len(categories_links)):
+        print(keys_list[i])
+        req = requests.get(categories_links[i])
+        soup = BeautifulSoup(req.content, 'lxml')
+        productlist = soup.find_all('div', class_='Okno OknoRwd')
+        category_products = []
 
-soup = BeautifulSoup(req.content, 'lxml')
-productlist = soup.find_all('div', class_='Okno OknoRwd')
-productlinks = []
-lamps = []
+        for item in productlist:
+            link = item.find_all('a', href=True)[0]
+            productlinks.append(link['href'])
+            itemreq = requests.get(link['href'])
+            itemsoup = BeautifulSoup(itemreq.content, 'lxml')
 
-for item in productlist:
-	link = item.find_all('a', href=True)[0]
-	productlinks.append(link['href'])
-	itemreq = requests.get(link['href'])
-	itemsoup = BeautifulSoup(itemreq.content, 'lxml')
+            if not itemsoup:
+                break
 
-	name = itemsoup.find('h1', itemprop='name').get_text(strip=True)
-	producent = itemsoup.find('div', itemprop='brand').get_text(strip=True)
-	catalognumber = itemsoup.find('strong', itemprop='mpn').get_text(strip=True)
-	price = itemsoup.find('span', itemprop='price')['content']
-	inInventory = int(itemsoup.find('p', id="StanMagazynowy").find('img')['src'][46]) * 2
-	picture = baseurl + '/' + itemsoup.find('img', itemprop='image')['src']
-	description = itemsoup.find('div', itemprop='description').get_text(strip=True)
-	description.replace(u'\xa0', u' ')
+            name = itemsoup.find('h1', itemprop='name').get_text(strip=True)
+            catalognumber = itemsoup.find('strong', itemprop='mpn').get_text(strip=True)
+            price = itemsoup.find('span', itemprop='price')['content']
+            inInventory = int(itemsoup.find('p', id="StanMagazynowy").find('img')['src'][46]) * 2
+            picture = baseurl + '/' + itemsoup.find('img', itemprop='image')['src']
+            description = itemsoup.find('div', itemprop='description').get_text(strip=True)
+            description.replace(u'\xa0', u' ')
 
-	lamp = {
-		'name': name,
-		'producent': producent,
-		'catalognumber': catalognumber,
-		'price': price,
-		'inInventory': inInventory,
-		'picture': picture,
-		'description': description,
-		'specs': 'Wysokość:' + findSpec('Wysokość:') + ',' +
-		'Szerokość:' + findSpec('Szerokość:') + ',' +
-		'Materiał:' + findSpec('Materiał:') + ',' +
-		'Kolor:' + findSpec('Kolor podstawowy:') + ',' +
-		'Trzonek:' + findSpec('Trzonek:') + ',' +
-		'Moc żarówki:' + findSpec('Moc żarówki:') + ',' +
-		'Napięcie zasilania:' + findSpec('Napięcie zasilania:') + ',' +
-		'Ilość żarówek:' + findSpec('Ilość żarówek:') + ',' +
-		'Kompatybilna z LED:' + findSpec('Kompatybilna z LED:') + ',' +
-		'Źródło światła w komplecie:' + findSpec('Źródło światła w komplecie:') + ',' +
-		'Klasa szczelności:' + findSpec('Klasa szczelności:')
-	}
-	print(lamp)
-	lamps.append(lamp)
+            lamp = {
+                'name': name,
+                'catalognumber': catalognumber,
+                'price': price,
+                'inInventory': inInventory,
+                'picture': picture,
+                'description': description,
+                'specs': [{'Wysokość': findSpec(itemsoup, 'Wysokość:')},
+                          {'Szerokość': findSpec(itemsoup, 'Szerokość:')},
+                          {'Materiał': findSpec(itemsoup, 'Materiał:')},
+                          {'Kolor': findSpec(itemsoup, 'Kolor podstawowy:')},
+                          {'Trzonek': findSpec(itemsoup, 'Trzonek:')},
+                          {'Moc żarówki': findSpec(itemsoup, 'Moc żarówki:')},
+                          {'Napięcie zasilania': findSpec(itemsoup, 'Napięcie zasilania:')},
+                          {'Ilość żarówek': findSpec(itemsoup, 'Ilość żarówek:')},
+                          {'Kompatybilna z LED': findSpec(itemsoup, 'Kompatybilna z LED:')},
+                          {'Źródło światła w komplecie': findSpec(itemsoup, 'Źródło światła w komplecie:')},
+                          {'Klasa szczelności': findSpec(itemsoup, 'Klasa szczelności:')}]
+            }
+            category_products.append(lamp)
+        lamps[keys_list[i]] = category_products
 
-df = pd.DataFrame(lamps)
-df.to_csv('scrapLamps.csv', sep=';', encoding='utf-8-sig')
 
-print(lamps)
+def save_lamps(lamps):
+    with open('scrap_data.json', 'w') as file:
+        json.dump(lamps, file)
+
+
+def scrap():
+    baseurl = "https://polskielampy.pl"
+    lamps = {}
+    categories_links = []
+    get_categories(lamps, baseurl, categories_links)
+    get_products(baseurl, lamps, categories_links)
+    save_lamps(lamps)
+
+
+if __name__ == '__main__':
+    scrap()
