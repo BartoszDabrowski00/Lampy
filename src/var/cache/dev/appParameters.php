d5857c0409e66f4e6e8d7f16fb7c84aff8c52637
Bartek <bartosz.dabrowski2000@gmail.com>
2021-11-20 19:39:27 +0100

    TestKonenera

---
+++ src/var/cache/dev/appParameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'lampy_presta_sql_1',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'admin',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'O1gXL64k0eq0K5h9AgEk6sCMLwO0T8yyii1AJP5gx2oyePeuzdFEZxtM0pOB1R6t',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2021-11-20',
+    'locale' => 'pl-PL',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'dHy5vXCU6PDg3OeWy91x2ephcNj9vZINcv98JA4S6bkINOAWXB5sUguKxteXL3if',
+    'cookie_iv' => 'RFXyWeHq5ys3bOZyfrkRhEMhpKftV349',
+    'new_cookie_key' => 'def00000c2f8b6a0398c18a867940e6c6d3171134eb9e89ed74169a07b286a7ba98ab3dacd3e5c79cd5fdae243e9bb9228c5f88729d77d99678d9587c99b659afc46fbf4',
+  ),
+);
