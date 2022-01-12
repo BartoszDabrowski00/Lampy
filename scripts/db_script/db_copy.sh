89088cd4279b00a38adc2fd4c68bc28d5e6bc222
BartoszDabrowski00 <69242805+BartoszDabrowski00@users.noreply.github.com>
2022-01-12 20:47:03 +0100

    Added db creation scripts

---
+++ scripts/db_script/db_copy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker cp ./BE_179962_abba.sql admin-mysql_db:/tmp/BE_179962_abba.sql
+
+docker cp ./db_script.sh admin-mysql_db:/tmp/db_script.sh
+
+docker exec admin-mysql_db chmod +x /tmp/db_script.sh
+
+docker exec admin-mysql_db /tmp/db_script.sh
+
+docker exec admin-mysql_db rm /tmp/BE_179962_abba.sql
+
+docker exec admin-mysql_db rm /tmp/db_script.sh
